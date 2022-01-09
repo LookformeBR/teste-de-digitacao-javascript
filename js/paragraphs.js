e12d0fe925d5d9b34deeda0ff69cd3d523e75523
Lucas Hilario Santos <lucashilarie@gmail.com>
2022-01-09 17:27:01 -0300

    Jogo de Digitação

---
+++ js/paragraphs.js
@@ -0,0 +1,15 @@
+const paragraphs = [
+    "As pessoas trabalham melhor quando sabem qual é o objetivo e o porquê. É importante que as pessoas estejam ansiosas para vir trabalhar de manhã e gostem de trabalhar. - Elon Musk",
+    "Não confunda diploma com conhecimento. Não fui para Harvard, mas as pessoas que trabalham para mim sim. - Elon Musk",
+    "Deixem que o futuro diga a verdade e avalie cada um de acordo com o seu trabalho e realizações. O presente pertence a eles, mas o futuro pelo qual eu sempre trabalhei pertence a mim. - Nikola Tesla",
+    "Não creio que haja uma emoção, mais intensa para um inventor do que ver suas criações funcionando. Essa emoção faz você esquecer de comer, de dormir, de tudo. - Nikola Tesla",
+    "Meus filhos terão computadores, sim, mas antes terão livros. Sem livros, sem leitura, os nossos filhos serão incapazes de escrever – inclusive a sua própria história. - Bill Gates",
+    "A primeira regra de qualquer tecnologia utilizada nos negócios é que a automação aplicada a uma operação eficiente aumentará a eficiência. A segunda é que a automação aplicada a uma operação ineficiente aumentará a ineficiência. - Bill Gates",
+    "Não deixe o barulho da opinião dos outros abafar sua voz interior. E mais importante, tenha a coragem de seguir seu coração e sua intuição. Eles de alguma forma já sabem o que você realmente quer se tornar. Tudo o mais é secundário. - Steve Jobs",
+    "Não faz sentido olhar para trás e pensar: devia ter feito isso ou aquilo, devia ter estado lá. Isso não importa. Vamos inventar o amanhã e parar de nos preocupar com o passado. - Steve Jobs",
+    "A educação é claramente o fator que irá conduzir melhorias na economia a longo prazo. No futuro, software e tecnologia irão permitir que as pessoas aprendam muito com seus colegas. - Steve Jobs",
+    "A nossa missão é dar às pessoas o poder de partilhar e tornar o mundo mais aberto e conetado. Precisamos de encontrar os melhores talentos para chegar lá. - Steve Jobs",
+    "Se você não for obstinado, você vai desistir de experiências muito cedo. E se você não for flexível, você vai bater com a cabeça na parede e não vai ver uma solução diferente para um problema que está tentando resolver. - Jeff Bezos",
+    "Se você está focado na competição, você terá que esperar até que um competidor faça algo. Se você está focado nos clientes, poderá ser pioneiro. - Jeff Bezos",
+
+];
